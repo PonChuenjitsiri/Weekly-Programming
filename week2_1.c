531c6a69152c977a0cb3c8259dbeba7d0e89bddd
Pon <pon_pon_47@hotmail.com>
2022-08-28 15:20:09 +0700

    programming week2

---
+++ week2_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2, num3, num4, a = 0;
+    scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
+    if (num1 % 2 == 0) {
+        a = a + 1;
+    }
+    if (num2 % 2 == 0) {
+        a = a + 1;
+    }
+    if (num3 % 2 == 0) {
+        a = a + 1;
+    }
+    if (num4 % 2 == 0) {
+        a = a + 1;
+        printf("%d", a);
+    }
+    return 0;
+}
